a3552406683ad2b66bddae45172308c1edb9bed7
rashmiranjan.das <rashmi.rrdas@gmail.com>
2021-11-03 09:10:20 +0530

    multiple consumer & producer

---
+++ src/test/java/com/multipleConsumerProducer/demo/DemoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multipleConsumerProducer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
